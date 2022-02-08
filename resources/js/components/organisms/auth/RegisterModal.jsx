a4e564d6b4090c792a2f83b9ca3d8af63ce05efa
EC2 Default User <ec2-user@ip-172-31-19-146.ap-northeast-1.compute.internal>
2022-02-09 01:17:15 +0900

    RegisterButton押下時の挙動追加 現時点では画面遷移のみ

--- resources/js/components/organisms/auth/RegisterModal.jsx
+++ resources/js/components/organisms/auth/RegisterModal.jsx
@@ -3,12 +3,17 @@ import {ButtonModal} from "../../molecules/ButtonModal";
 import {Input} from "../../atoms/input/Input";
 import {TextInput} from "../../molecules/TextInput";
 import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
 
 export const RegisterModal = (props) => {
     const {isOpen,setIsOpen} = props;
-
+    const navigate = useNavigate();
+    const onClickRegister = () => {
+        navigate("/select");
+    }
+    
     return(
-        <ButtonModal isOpen={isOpen} text={"SEND"} setIsOpen={setIsOpen} sizeHeight={"20vh"} sizeWidth={"37vw"}>
+        <ButtonModal isOpen={isOpen} text={"SEND"} setIsOpen={setIsOpen} sizeHeight={"20vh"} sizeWidth={"37vw"} onClickEvent={onClickRegister}>
             <h2 style={{color:"white"}}>REGISTER</h2>
             <TextInput type={"text"} text={"USER NAME"} placeholder={"your name"} />
             <TextInput type={"email"} text={"EMAIL ADRESS"} placeholder={"example@gmail.lc"} />
