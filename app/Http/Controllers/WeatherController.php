5c64cd7f69b80fe1355d7ff1de0389f46f54f130
EC2 Default User <ec2-user@ip-172-31-19-146.ap-northeast-1.compute.internal>
2022-02-13 17:32:42 +0900

    外部APIとの通信機能を追加 天気に関する情報を取得するAPI

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
 
 class WeatherController extends Controller
 {
     public function getWeatherData(Request $request){
-      logger($request);
-      $client = new \GuzzleHttp\Client();
-      $base_url = "http://api.openweathermap.org/data/2.5/forecast/"
-      
-      
-      return response()->json(["weather_data" => $request->location_id]);
+        $location_id = $request["location_id"];
+        $client = new Client();
+        $base_url = "http://api.openweathermap.org/data/2.5/forecast/";
+        $api_key = config("services.weather_forecast.key");
+        $url = $base_url . "?id=" . $location_id . "&lang=ja&units=metric&appid=" . $api_key;
+        $response = $client->request("GET", $url);
+        $weather_data = $response->getBody();
+        $weather_data = json_decode($weather_data, true);
+        
+        return response()->json(["weather_data" => $weather_data]);
    }
 }
